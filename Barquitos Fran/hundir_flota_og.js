93f8d7925c2d831837bb5127721e8e0f0f0abbf9
Franciskis <63367727+Franciskis@users.noreply.github.com>
2021-11-15 14:07:41 +0100

    barcos progresan

--- Barquitos Fran/hundir_flota_og.js
+++ Barquitos Fran/hundir_flota_og.js
@@ -21,10 +21,8 @@ window.onload = function() {
     nuevoTablero(); //pone a cero
     //console.log(tablero);
     pintaTablero();
-
-
-    //ubicaBarcos(numeroBarcos, tamanoBarcos);
-
+    ubicaBarcos(numeroBarcos, tamanoBarcos);
+    pintaTablero();
 
 
 }
@@ -43,26 +41,64 @@ function nuevoTablero() {
 
 function ubicaBarcos(numeroBarcos, tamanoBarcos) {
     numeroBarcos.forEach(function(cantidad, tipo) {
-
-
-        //posXini = (parseInt(Math.random() * (10 - tamaño)));
-
-
-
-
-
-
-
-
-
-
+        let size = tamanoBarcos.get(tipo);
+        amenizaje(cantidad, size);
     });
+}
 
-    //para añadir css directamente    document.getElementById(`id_${x}_${y}`).style.backgroundColor="grey";
+function esVertical() {
+    let r = parseInt(Math.random() * 2);
+    if (r == 0) {
+        return false;
+    } else if (r == 1) {
+        return true;
+    }
+}
 
+function posX(sentido, size) {
+    if (sentido) {
+        return parseInt(Math.random() * 10);
+    } else {
+        return parseInt(Math.random() * (10 - size));
+    }
+}
 
+function posY(sentido, size) {
+    if (sentido) {
+        return parseInt(Math.random() * (10 - size));
+    } else {
+        return parseInt(Math.random() * 10);
+    }
+}
 
+function comprobar(x, y, sentido, size) {
+    //Hacer luego
+    return true;
+}
 
+function lanzar(x, y, sentido, size) {
+    if (!sentido) {
+        for (let i = 0; i < size; i++, x++) {
+            tablero[`id_${x}_${y}`] = 1;
+        }
+    } else if (sentido) {
+        for (let i = 0; i < size; i++, y++) {
+            tablero[`id_${x}_${y}`] = 1;
+        }
+    }
+}
+
+function amenizaje(cantidad, size) {
+    let sentido, x, y;
+    for (let i = 0; i < cantidad; i++) {
+        do {
+            sentido = esVertical();
+            x = posX(sentido, size);
+            y = posY(sentido, size);
+        }
+        while (!comprobar(x, y, sentido, size));
+        lanzar(x, y, sentido, size);
+    }
 }
 
 function pintaTablero() {
@@ -73,6 +109,9 @@ function pintaTablero() {
             switch (tablero[`id_${x}_${y}`]) {
                 case 0:
                     document.getElementById(`id_${x}_${y}`).style.backgroundColor = "lightblue";
+                    break;
+                case 1:
+                    document.getElementById(`id_${x}_${y}`).style.backgroundColor = "grey";
                     break;
             }
 
